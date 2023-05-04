c78448b9cdc2f573d811b87d9fb2093edd3bcefb
Ilya Afanasyev <affeeal@yandex.ru>
2023-05-04 14:35:15 +0300

    Написаны тесты CalendarSettingsFM

--- lib/calendar_settings_fm/tests/src/calendar_settings_fm_tests.cpp
+++ lib/calendar_settings_fm/tests/src/calendar_settings_fm_tests.cpp
@@ -1,7 +1,46 @@
+#include <algorithm>
 #include <gtest/gtest.h>
 
+#include "calendar.hpp"
 #include "calendar_settings_fm.hpp"
 
-TEST(EventTest, GTestUsageFailed) {
-  EXPECT_EQ(1, 1);
+const std::string kEmpty = "";
+const std::string kTitle = "title";
+const std::string kDescription = "description";
+const std::string kColor = "#000000";
+const std::string kVisibility = "public";
+
+TEST(CalendarSettingsFMTest, EmptyFailed) {
+  auto calendar = std::make_unique<Calendar>();
+  auto calendar_settings_fm = std::make_unique<CalendarSettingsFM>();
+  calendar_settings_fm->SetData(std::move(calendar));
+
+  EXPECT_FALSE(calendar_settings_fm->validate());
+}
+
+TEST(CalendarSettingsFMTest, EmptyTitleFailed) {
+  auto calendar = std::make_unique<Calendar>(
+      kEmpty, kDescription, kColor, kVisibility);
+  auto calendar_settings_fm = std::make_unique<CalendarSettingsFM>();
+  calendar_settings_fm->SetData(std::move(calendar));
+
+  EXPECT_FALSE(calendar_settings_fm->validate());
+}
+
+TEST(CalendarSettingsFMTest, EmptyDescriptionOk) {
+  auto calendar = std::make_unique<Calendar>(
+      kTitle, kEmpty, kColor, kVisibility);
+  auto calendar_settings_fm = std::make_unique<CalendarSettingsFM>();
+  calendar_settings_fm->SetData(std::move(calendar));
+
+  EXPECT_TRUE(calendar_settings_fm->validate());
+}
+
+TEST(CalendarSettingsFMTest, AllFieldsOk) {
+  auto calendar = std::make_unique<Calendar>(
+      kTitle, kDescription, kColor, kVisibility);
+  auto calendar_settings_fm = std::make_unique<CalendarSettingsFM>();
+  calendar_settings_fm->SetData(std::move(calendar));
+
+  EXPECT_TRUE(calendar_settings_fm->validate());
 }
