5444467aec599eb611cc5ddc79301d003b2aea46
Ilya Afanasyev <affeeal@yandex.ru>
2023-05-15 14:38:36 +0300

    Дописаны тесты парсера

--- src/calendula/parser/parser.test.cpp
+++ src/calendula/parser/parser.test.cpp
@@ -7,6 +7,9 @@
 
 #include "parser.hpp"
 
+// TODO(affeeal): вынести StringStubCharReader и FileCharReader
+// в один класс
+
 class StringStubCharReader : public ICharReader {
  public:
   char Peek(std::size_t k = 0) override {
@@ -55,8 +58,8 @@ TEST_F(iCalendarParserTest, EmptyStream) {
 
   StreamUptr root = parser_.Parse();
 
-  std::vector<ComponentUptr>& stream_comps = root->components();
-  EXPECT_EQ(stream_comps.size(), 0);
+  std::vector<ComponentUptr>& stream_components = root->components();
+  ASSERT_EQ(stream_components.size(), 0);
 }
 
 TEST_F(iCalendarParserTest, EmptyCalendar) {
@@ -66,15 +69,15 @@ TEST_F(iCalendarParserTest, EmptyCalendar) {
 
   StreamUptr root = parser_.Parse();
 
-  std::vector<ComponentUptr>& stream_comps = root->components();
-  ASSERT_EQ(stream_comps.size(), 1);
+  std::vector<ComponentUptr>& stream_components = root->components();
+  ASSERT_EQ(stream_components.size(), 1);
 
-  ComponentUptr& cal_0 = stream_comps[0];
-  ASSERT_TRUE(cal_0);
+  ComponentUptr& calendar = stream_components[0];
+  ASSERT_TRUE(calendar);
 
-  EXPECT_EQ(cal_0->name(), "VCALENDAR");
-  EXPECT_TRUE(cal_0->properties().empty());
-  EXPECT_TRUE(cal_0->components().empty());
+  EXPECT_EQ(calendar->name(), "VCALENDAR");
+  EXPECT_TRUE(calendar->components().empty());
+  EXPECT_TRUE(calendar->properties().empty());
 }
 
 TEST_F(iCalendarParserTest, EmptyCalendars) {
@@ -86,93 +89,70 @@ TEST_F(iCalendarParserTest, EmptyCalendars) {
 
   StreamUptr root = parser_.Parse();
 
-  std::vector<ComponentUptr>& stream_comps = root->components();
-  ASSERT_EQ(stream_comps.size(), 2);
+  std::vector<ComponentUptr>& stream_components = root->components();
+  ASSERT_EQ(stream_components.size(), 2);
 
-  ComponentUptr& cal_0 = stream_comps[0];
-  ASSERT_TRUE(cal_0);
+  for (ComponentUptr& calendar : stream_components) {
+    ASSERT_TRUE(calendar);
 
-  EXPECT_EQ(cal_0->name(), "VCALENDAR");
-  EXPECT_TRUE(cal_0->properties().empty());
-  EXPECT_TRUE(cal_0->components().empty());
-
-  ComponentUptr& cal_1 = stream_comps[1];
-  ASSERT_TRUE(cal_1);
-
-  EXPECT_EQ(cal_1->name(), "VCALENDAR");
-  EXPECT_TRUE(cal_1->properties().empty());
-  EXPECT_TRUE(cal_1->components().empty());
-}
-
-TEST_F(iCalendarParserTest, CalendarWithProperty) {
-  std::stringstream ss("BEGIN:VCALENDAR\r\n"
-                       "VERSION:2.0\r\n"
-                       "END:VCALENDAR\r\n");
-  char_reader_.set_ss(std::move(ss));
-
-  StreamUptr root = parser_.Parse();
-
-  std::vector<ComponentUptr>& stream_comps = root->components();
-  ASSERT_EQ(stream_comps.size(), 1);
-
-  ComponentUptr& cal_0 = stream_comps[0];
-  ASSERT_TRUE(cal_0);
-
-  std::vector<PropertyUptr>& cal_0_props = cal_0->properties();
-  ASSERT_EQ(cal_0_props.size(), 1) ;
-
-  PropertyUptr& cal_0_prop_0 = cal_0_props[0];
-  ASSERT_TRUE(cal_0_prop_0);
-
-  EXPECT_EQ(cal_0_prop_0->name(), "VERSION");
-  IValueUptr& value_0 = cal_0_prop_0->value();
-  ASSERT_TRUE(value_0);
-  TextValue* value_0_text = dynamic_cast<TextValue*>(value_0.get());
-  ASSERT_TRUE(value_0_text);
-  EXPECT_EQ(value_0_text->text(), "2.0");
+    EXPECT_EQ(calendar->name(), "VCALENDAR");
+    EXPECT_TRUE(calendar->components().empty());
+    EXPECT_TRUE(calendar->properties().empty());
+  }
 }
 
 TEST_F(iCalendarParserTest, CalendarWithProperties) {
   std::stringstream ss("BEGIN:VCALENDAR\r\n"
                        "VERSION:2.0\r\n"
-                       "CALSCALE:GREGORIAN\r\n"
+                       "PRODID:students.bmstu.ru\r\n"
                        "END:VCALENDAR\r\n");
   char_reader_.set_ss(std::move(ss));
 
   StreamUptr root = parser_.Parse();
 
-  std::vector<ComponentUptr>& stream_comps = root->components();
-  ASSERT_EQ(stream_comps.size(), 1);
+  std::vector<ComponentUptr>& stream_components = root->components();
+  ASSERT_EQ(stream_components.size(), 1);
+  
+  ComponentUptr& calendar = stream_components[0];
+  ASSERT_TRUE(calendar);
 
-  ComponentUptr& cal_0 = stream_comps[0];
-  ASSERT_TRUE(cal_0);
+  std::vector<PropertyUptr>& calendar_properties = calendar->properties();
+  ASSERT_EQ(calendar_properties.size(), 2) ;
 
-  std::vector<PropertyUptr>& cal_0_props = cal_0->properties();
-  ASSERT_EQ(cal_0_props.size(), 2) ;
+  std::queue<std::pair<std::string, IValueUptr>> expected;
+  expected.push(std::make_pair(
+        "VERSION", std::make_unique<TextValue>("2.0")));
+  expected.push(std::make_pair(
+        "PRODID", std::make_unique<TextValue>("students.bmstu.ru")));
+  
+  for (PropertyUptr& property : calendar_properties) {
+    std::string expected_name = std::move(expected.front().first);
+    IValueUptr expected_value = std::move(expected.front().second);
+    expected.pop();
 
-  PropertyUptr& cal_0_prop_0 = cal_0_props[0];
-  ASSERT_TRUE(cal_0_prop_0);
+    TextValue* expected_text_value
+      = dynamic_cast<TextValue*>(expected_value.get());
+    ASSERT_TRUE(expected_text_value);
 
-  EXPECT_EQ(cal_0_prop_0->name(), "VERSION");
-  IValueUptr& value_0 = cal_0_prop_0->value();
-  ASSERT_TRUE(value_0);
-  TextValue* value_0_text = dynamic_cast<TextValue*>(value_0.get());
-  ASSERT_TRUE(value_0_text);
-  EXPECT_EQ(value_0_text->text(), "2.0");
+    ASSERT_TRUE(property);
+    EXPECT_EQ(property->name(), expected_name);
+    EXPECT_TRUE(property->parameters().empty());
 
-  PropertyUptr& cal_0_prop_1 = cal_0_props[1];
-  ASSERT_TRUE(cal_0_prop_1);
+    IValueUptr& value = property->value();
+    ASSERT_TRUE(value);
 
-  EXPECT_EQ(cal_0_prop_1->name(), "CALSCALE");
-  IValueUptr& value_1 = cal_0_prop_1->value();
-  ASSERT_TRUE(value_1);
-  TextValue* value_1_text = dynamic_cast<TextValue*>(value_1.get());
-  ASSERT_TRUE(value_1_text);
-  EXPECT_EQ(value_1_text->text(), "GREGORIAN");
+    TextValue* text_value = dynamic_cast<TextValue*>(value.get());
+    ASSERT_TRUE(text_value);
+    EXPECT_EQ(text_value->text(), expected_text_value->text());
+  }
+
+  EXPECT_TRUE(calendar->components().empty());
 }
 
 TEST_F(iCalendarParserTest, CalendarWithEmptyEvent) {
   std::stringstream ss("BEGIN:VCALENDAR\r\n"
+                       "VERSION:2.0\r\n"
+                       "PRODID:students.bmstu.ru\r\n"
                        "BEGIN:VEVENT\r\n"
                        "END:VEVENT\r\n"
                        "END:VCALENDAR\r\n");
@@ -180,24 +160,57 @@ TEST_F(iCalendarParserTest, CalendarWithEmptyEvent) {
 
   StreamUptr root = parser_.Parse();
 
-  std::vector<ComponentUptr>& stream_comps = root->components();
-  ASSERT_EQ(stream_comps.size(), 1);
+  std::vector<ComponentUptr>& stream_components = root->components();
+  ASSERT_EQ(stream_components.size(), 1);
   
-  ComponentUptr& cal_0 = stream_comps[0];
-  ASSERT_TRUE(cal_0);
+  ComponentUptr& calendar = stream_components[0];
+  ASSERT_TRUE(calendar);
 
-  std::vector<ComponentUptr>& cal_0_comps = cal_0->components();
-  ASSERT_EQ(cal_0_comps.size(), 1) ;
+  std::vector<PropertyUptr>& calendar_properties = calendar->properties();
+  ASSERT_EQ(calendar_properties.size(), 2) ;
 
-  ComponentUptr& cal_0_event_0 = cal_0_comps[0];
-  ASSERT_TRUE(cal_0_event_0);
-  EXPECT_EQ(cal_0_event_0->name(), "VEVENT");
-  EXPECT_TRUE(cal_0_event_0->components().empty());
-  EXPECT_TRUE(cal_0_event_0->properties().empty());
+  std::queue<std::pair<std::string, IValueUptr>> expected;
+  expected.push(std::make_pair(
+        "VERSION", std::make_unique<TextValue>("2.0")));
+  expected.push(std::make_pair(
+        "PRODID", std::make_unique<TextValue>("students.bmstu.ru")));
+  
+  for (PropertyUptr& property : calendar_properties) {
+    std::string expected_name = std::move(expected.front().first);
+    IValueUptr expected_value = std::move(expected.front().second);
+    expected.pop();
+
+    TextValue* expected_text_value
+      = dynamic_cast<TextValue*>(expected_value.get());
+    ASSERT_TRUE(expected_text_value);
+
+    ASSERT_TRUE(property);
+    EXPECT_EQ(property->name(), expected_name);
+    EXPECT_TRUE(property->parameters().empty());
+
+    IValueUptr& value = property->value();
+    ASSERT_TRUE(value);
+
+    TextValue* text_value = dynamic_cast<TextValue*>(value.get());
+    ASSERT_TRUE(text_value);
+    EXPECT_EQ(text_value->text(), expected_text_value->text());
+  }
+
+  std::vector<ComponentUptr>& calendar_components = calendar->components();
+  ASSERT_EQ(calendar_components.size(), 1) ;
+
+  ComponentUptr& event = calendar_components[0];
+  ASSERT_TRUE(event);
+
+  EXPECT_EQ(event->name(), "VEVENT");
+  EXPECT_TRUE(event->properties().empty());
+  EXPECT_TRUE(event->components().empty());
 }
 
 TEST_F(iCalendarParserTest, CalendarWithEmptyEvents) {
   std::stringstream ss("BEGIN:VCALENDAR\r\n"
+                       "VERSION:2.0\r\n"
+                       "PRODID:students.bmstu.ru\r\n"
                        "BEGIN:VEVENT\r\n"
                        "END:VEVENT\r\n"
                        "BEGIN:VEVENT\r\n"
@@ -207,26 +220,52 @@ TEST_F(iCalendarParserTest, CalendarWithEmptyEvents) {
 
   StreamUptr root = parser_.Parse();
 
-  std::vector<ComponentUptr>& stream_comps = root->components();
-  ASSERT_EQ(stream_comps.size(), 1);
+  std::vector<ComponentUptr>& stream_components = root->components();
+  ASSERT_EQ(stream_components.size(), 1);
   
-  ComponentUptr& cal_0 = stream_comps[0];
-  ASSERT_TRUE(cal_0);
+  ComponentUptr& calendar = stream_components[0];
+  ASSERT_TRUE(calendar);
 
-  std::vector<ComponentUptr>& cal_0_comps = cal_0->components();
-  ASSERT_EQ(cal_0_comps.size(), 2) ;
+  std::vector<PropertyUptr>& calendar_properties = calendar->properties();
+  ASSERT_EQ(calendar_properties.size(), 2) ;
 
-  ComponentUptr& cal_0_event_0 = cal_0_comps[0];
-  ASSERT_TRUE(cal_0_event_0);
-  EXPECT_EQ(cal_0_event_0->name(), "VEVENT");
-  EXPECT_TRUE(cal_0_event_0->components().empty());
-  EXPECT_TRUE(cal_0_event_0->properties().empty());
+  std::queue<std::pair<std::string, IValueUptr>> expected;
+  expected.push(std::make_pair(
+        "VERSION", std::make_unique<TextValue>("2.0")));
+  expected.push(std::make_pair(
+        "PRODID", std::make_unique<TextValue>("students.bmstu.ru")));
+  
+  for (PropertyUptr& property : calendar_properties) {
+    std::string expected_name = std::move(expected.front().first);
+    IValueUptr expected_value = std::move(expected.front().second);
+    expected.pop();
 
-  ComponentUptr& cal_0_event_1 = cal_0_comps[1];
-  ASSERT_TRUE(cal_0_event_1);
-  EXPECT_EQ(cal_0_event_1->name(), "VEVENT");
-  EXPECT_TRUE(cal_0_event_1->components().empty());
-  EXPECT_TRUE(cal_0_event_1->properties().empty());
+    TextValue* expected_text_value
+      = dynamic_cast<TextValue*>(expected_value.get());
+    ASSERT_TRUE(expected_text_value);
+
+    ASSERT_TRUE(property);
+    EXPECT_EQ(property->name(), expected_name);
+    EXPECT_TRUE(property->parameters().empty());
+
+    IValueUptr& value = property->value();
+    ASSERT_TRUE(value);
+
+    TextValue* text_value = dynamic_cast<TextValue*>(value.get());
+    ASSERT_TRUE(text_value);
+    EXPECT_EQ(text_value->text(), expected_text_value->text());
+  }
+
+  std::vector<ComponentUptr>& calendar_components = calendar->components();
+  ASSERT_EQ(calendar_components.size(), 2) ;
+
+  for (ComponentUptr& event : calendar_components) {
+    ASSERT_TRUE(event);
+
+    EXPECT_EQ(event->name(), "VEVENT");
+    EXPECT_TRUE(event->properties().empty());
+    EXPECT_TRUE(event->components().empty());
+  }
 }
 
 TEST_F(iCalendarParserTest, CalendarWithEvents) {
