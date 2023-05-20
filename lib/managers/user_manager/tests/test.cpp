533b0cfc5311dc3fe62215d935aa9da3dc75ee37
Arseniy Bannikov <bannikovtop@gmail.com>
2023-05-20 19:35:08 +0300

    Фикс тестов

--- lib/managers/user_manager/tests/test.cpp
+++ lib/managers/user_manager/tests/test.cpp
@@ -1,16 +1,33 @@
 #include <gtest/gtest.h>
 #include <memory>
 
+#include "CalendarDbManagerMock.hpp"
+#include "CommentDbManagerMock.hpp"
+#include "DirectoryDbManagerMock.hpp"
+#include "EventDbManagerMock.hpp"
+#include "NodeDbManagerMock.hpp"
+#include "ProfileDbManagerMock.hpp"
+#include "TagDbManagerMock.hpp"
 #include "User.hpp"
 #include "DbMock.hpp"
-#include "IDbManagers.hpp"
+#include "DbManagers.hpp"
 #include "IManagers.hpp"
+#include "UserDbManagerMock.hpp"
 
 class DbSuit : public ::testing::Test {
 protected:
 	void SetUp() override {
 		std::shared_ptr<DbMock> db = std::make_shared<DbMock>();
-		db_managers = std::make_shared<IDbManagers>();
+		db_managers = std::make_shared<DbManagers>(
+			std::make_unique<UserDbManagerMock>(db),
+			std::make_unique<NodeDbManagerMock>(db),
+			std::make_unique<DirectoryDbManagerMock>(db),
+			std::make_unique<CalendarDbManagerMock>(db),
+			std::make_unique<EventDbManagerMock>(db),
+			std::make_unique<CommentDbManagerMock>(),
+			std::make_unique<TagDbManagerMock>(),
+			std::make_unique<ProfileDbManagerMock>()
+		);
 	}
 
 	void TearDown() override {}
